42ac8980846ff9d28fd65a8f051626372dd90db0
Flatout01 <lingyi.s@outlook.com>
2020-05-05 23:48:16 -0400

    Update wgan.py

--- GANs/wgan.py
+++ GANs/wgan.py
@@ -11,10 +11,7 @@ from numpy import iscomplexobj
 from numpy import asarray
 from numpy.random import shuffle
 from scipy.linalg import sqrtm
-from keras.applications.inception_v3 import InceptionV3
-from keras.applications.inception_v3 import preprocess_input
 from skimage.transform import resize
-from keras.datasets import cifar10
 from numpy import asarray
 from numpy import expand_dims
 from numpy import log
@@ -44,26 +41,6 @@ def scale_images(images, new_shape):
         images_list.append(new_image)
     return asarray(images_list)
  
-# calculate frechet inception distance
-def calculate_fid(model, images1, images2):
-    # calculate activations
-    act1 = model.predict(images1)
-    act2 = model.predict(images2)
-    # calculate mean and covariance statistics
-    mu1, sigma1 = act1.mean(axis=0), cov(act1, rowvar=False)
-    mu2, sigma2 = act2.mean(axis=0), cov(act2, rowvar=False)
-    # calculate sum squared difference between means
-    ssdiff = numpy.sum((mu1 - mu2)**2.0)
-    # calculate sqrt of product between cov
-    covmean = sqrtm(sigma1.dot(sigma2))
-    # check and correct imaginary numbers from sqrt
-    if iscomplexobj(covmean):
-        covmean = covmean.real
-    # calculate score
-    fid = ssdiff + trace(sigma1 + sigma2 - 2.0 * covmean)
-    fid = fid/10
-    return fid
-
 def batch_preprocess(data_batch):
 
     batch = sess.run(data_batch)
@@ -293,33 +270,3 @@ with tf.Session() as sess:
         plt.axis('off')
         plt.savefig("wgan_img/car_%d.png" % i)
         plt.show
-
-# conditional probabilities for high quality images
-p_yx = gen_samples[0]
-score = calculate_inception_score(p_yx)
-print('inception score of WGAN: %.3f' %score)
-
-# prepare the inception v3 model
-model = InceptionV3(include_top=False, pooling='avg', input_shape=(299,299,3))
-
-images2=gen_samples[0]
-
-# load cifar10 images
-(images1, _), (_, _) = cifar10.load_data()
-shuffle(images1)
-images1 = images1[:10]
-images2=images2[:10]
-print('Loaded', images1.shape, images2.shape)
-# convert integer to floating point values
-images1 = images1.astype('float32')
-images2 = images2.astype('float32')
-# resize images
-images1 = scale_images(images1, (299,299,3))
-images2 = scale_images(images2, (299,299,3))
-print('Scaled', images1.shape, images2.shape)
-# pre-process images
-images1 = preprocess_input(images1)
-images2 = preprocess_input(images2)
-# calculate fid
-fid = calculate_fid(model, images1, images2)
-print('FID of WGAN: %.3f' % fid)
